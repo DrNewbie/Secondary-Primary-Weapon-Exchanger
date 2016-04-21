ff958f91366008114512cc04983b462e3e0cfdb8
DrNewbie <tf2baidonation@gmail.com>
2016-04-22 01:37:51 +0800

    upup

--- lua/weaponfactorymanager.lua
+++
@@ -1,6 +0,0 @@
-if WeaponFactoryManager and tweak_data and tweak_data.statistics then
-	local _, _, _, weapon_list, _, _, _, _, _ = tweak_data.statistics:statistics_table()
-	for _, weapon_id in pairs(weapon_list) do
-	
-	end
-end
