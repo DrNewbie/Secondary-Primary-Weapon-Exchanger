706a33cde4b4f9c673ed98810445fb461fa3f990
Dr_Newbie <tf2baidonation@gmail.com>
2016-09-17 19:09:58 +0800

    8.1 ; temporarily fix

--- SecondaryPrimaryWeaponExchanger/lua/blackmarketgui.lua
+++
@@ -1,8 +0,0 @@
-_G.SecondaryPrimaryWeapon = _G.SecondaryPrimaryWeapon or {}
-
-local _f_BlackMarketGui_reload = BlackMarketGui.reload
-
-function BlackMarketGui:reload()
-	SecondaryPrimaryWeapon:_BlackMarketManager_load_done()
-	_f_BlackMarketGui_reload(self)
-end
--- lua/playerinventory.lua
+++ lua/playerinventory.lua
@@ -6,7 +6,7 @@ function PlayerInventory:add_unit(new_unit, is_equip, equip_is_instant)
 	new_selection.unit = new_unit
 	new_unit:base():add_destroy_listener(self._listener_id, callback(self, self, "clbk_weapon_unit_destroyed"))
 	local selection_index = use_data.selection_index
-	local _factory_id = new_unit:base()._factory_id
+	local _factory_id = new_unit:base()._factory_id or ""
 	if _factory_id:find("duplicate") then
 		use_data.selection_index = use_data.selection_index == 1 and 2 or 1
 	end
